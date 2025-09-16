1448daa9390055e6eb5e1029148d0d216cd6a0fe
Raul Fonseca <114365146+RaulFonseca002@users.noreply.github.com>
2025-09-16 15:58:50 -0300

    new problems

--- 1163.last-substring-in-lexicographical-order.cpp
+++ 1163.last-substring-in-lexicographical-order.cpp
@@ -10,8 +10,20 @@
 
 class Solution {
 public:
+
     std::string lastSubstring(std::string s) {
-        
+
+        int root = 0;
+        const int lenght = s.length();
+
+        for(int index = 0; index < lenght; index++){
+            
+            if(s.compare(root, lenght, s, index, lenght) < 0){
+                root = index;
+            }
+        }
+
+        return s.substr(root);
     }
 };
 // @lc code=end
---
+++ 813.largest-sum-of-averages.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=813 lang=cpp
+ *
+ * [813] Largest Sum of Averages
+ */
+
+// @lc code=start
+class Solution {
+public:
+    double largestSumOfAverages(vector<int>& nums, int k) {
+        
+    }
+};
+// @lc code=end
+
